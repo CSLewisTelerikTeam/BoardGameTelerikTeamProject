a2da9a93f91d4b9c728412889a8ac93ab6642328
Veselin Dimitrov <vidimitrov94@gmail.com>
2014-02-07 00:46:29 +0200

    WarChess UI added
    Basic UI of WarChess added. This a template and there's a lot of work to
    do.

--- BoardGame/MainWindow.xaml.cs
+++ BoardGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BoardGame
+namespace OOPGame_WoWChess
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +24,72 @@ namespace BoardGame
         public MainWindow()
         {
             InitializeComponent();
+
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource =
+            new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/bg.jpg", UriKind.Absolute));
+            this.Background = myBrush;
+
+            Image warchief = new Image();
+            warchief.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/warchief.png", UriKind.Absolute));
+            Image captain = new Image();
+            captain.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/captain.png", UriKind.Absolute));
+            Image commander = new Image();
+            commander.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/commander.png", UriKind.Absolute));
+            Image grunt = new Image();
+            grunt.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/grunt.png", UriKind.Absolute));
+            Image king = new Image();
+            king.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/king.png", UriKind.Absolute));
+            Image squire = new Image();
+            squire.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squire.png", UriKind.Absolute));
+
+            Img1.Source = warchief.Source;
+            Img2.Source = captain.Source;
+            Img3.Source = king.Source;
+            Img4.Source = squire.Source;
+            Img5.Source = commander.Source;
+            Img6.Source = warchief.Source;
+            Img7.Source = grunt.Source;
+            Img8.Source = king.Source;
+            Img11.Source = warchief.Source;
+            Img12.Source = captain.Source;
+            Img13.Source = king.Source;
+            Img14.Source = warchief.Source;
+            Img15.Source = commander.Source;
+            Img16.Source = warchief.Source;
+            Img17.Source = grunt.Source;
+            Img18.Source = king.Source;
+
+            //WarchiefCanvas.
+
+        }
+
+        private void WarchiefUnit_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/warchiefVoice.mp3"));
+            mediaPlayer.Play();
+        }
+
+        private void CaptainUnit_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/captainVoice.mp3"));
+            mediaPlayer.Play();
+        }
+
+        private void KingUnit_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/kingVoice.mp3"));
+            mediaPlayer.Play();
+        }
+
+        private void SquireUnit_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squireVoice.mp3"));
+            mediaPlayer.Play();
         }
     }
 }
