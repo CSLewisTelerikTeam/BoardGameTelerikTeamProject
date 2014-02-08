b5703600c2bb53d5304dc98e957db2d05dfa0807
Veselin Dimitrov <vidimitrov94@gmail.com>
2014-02-09 00:38:43 +0200

    Some UI changes
    OnHover event added to visualize the whole card. The playfield must be
    6x6, because when the field is 8x8 the mini variant of the card don't
    let you to read its characteristics (health, damage, level).

--- BoardGame/MainWindow.xaml.cs
+++ BoardGame/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BoardGame.UnitClasses;
+using System.Threading;
 
 namespace OOPGame_WoWChess
 {
@@ -27,41 +28,60 @@ namespace OOPGame_WoWChess
             InitializeComponent();
 
             ImageBrush myBrush = new ImageBrush();
-            myBrush.ImageSource =
-            new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/bg.jpg", UriKind.Absolute));
+            myBrush.ImageSource = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/bg1.png", UriKind.Absolute));
             this.Background = myBrush;
 
-            Image warchief = new Image();
-            warchief.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/warchief.png", UriKind.Absolute));
-            Image captain = new Image();
-            captain.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/captain.png", UriKind.Absolute));
-            Image commander = new Image();
-            commander.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/commander.png", UriKind.Absolute));
-            Image grunt = new Image();
-            grunt.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/grunt.png", UriKind.Absolute));
-            Image king = new Image();
-            king.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/king.png", UriKind.Absolute));
-            Image squire = new Image();
-            squire.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squire.png", UriKind.Absolute));
+            Image alianceSquare = new Image();
+            alianceSquare.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squareAliance.png", UriKind.Absolute));
+            Image hordeSquare = new Image();
+            hordeSquare.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squareHorde.png", UriKind.Absolute));
 
-            Img1.Source = warchief.Source;
-            Img2.Source = captain.Source;
-            Img3.Source = king.Source;
-            Img4.Source = squire.Source;
-            Img5.Source = commander.Source;
-            Img6.Source = warchief.Source;
-            Img7.Source = grunt.Source;
-            Img8.Source = king.Source;
-            Img11.Source = warchief.Source;
-            Img12.Source = captain.Source;
-            Img13.Source = king.Source;
-            Img14.Source = warchief.Source;
-            Img15.Source = commander.Source;
-            Img16.Source = warchief.Source;
-            Img17.Source = grunt.Source;
-            Img18.Source = king.Source;
+            //Image warchief = new Image();
+            //warchief.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/warchief.png", UriKind.Absolute));
+            //Image captain = new Image();
+            //captain.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/captain.png", UriKind.Absolute));
+            //Image commander = new Image();
+            //commander.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/commander.png", UriKind.Absolute));
+            //Image grunt = new Image();
+            //grunt.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/grunt.png", UriKind.Absolute));
+            //Image king = new Image();
+            //king.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/king.png", UriKind.Absolute));
+            //Image squire = new Image();
+            //squire.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squire.png", UriKind.Absolute));
 
-            //WarchiefCanvas.   
+            //Img1.Source = warchief.Source;
+            //Img2.Source = captain.Source;
+            //Img3.Source = king.Source;
+            //Img4.Source = squire.Source;
+            //Img5.Source = commander.Source;
+            //Img6.Source = warchief.Source;
+            ////Img7.Source = grunt.Source;
+            ////Img8.Source = king.Source;
+            //Img11.Source = warchief.Source;
+            //Img12.Source = captain.Source;
+            //Img13.Source = king.Source;
+            //Img14.Source = warchief.Source;
+            //Img15.Source = commander.Source;
+            //Img16.Source = warchief.Source;
+            ////Img17.Source = grunt.Source;
+            ////Img18.Source = king.Source;
+
+            Img1.Source = alianceSquare.Source;
+            Img2.Source = alianceSquare.Source;
+            Img3.Source = alianceSquare.Source;
+            Img4.Source = alianceSquare.Source;
+            Img5.Source = alianceSquare.Source;
+            Img6.Source = alianceSquare.Source;
+            Img7.Source = alianceSquare.Source;
+            Img8.Source = alianceSquare.Source;
+            Img11.Source = hordeSquare.Source;
+            Img12.Source = hordeSquare.Source;
+            Img13.Source = hordeSquare.Source;
+            Img14.Source = hordeSquare.Source;
+            Img15.Source = hordeSquare.Source;
+            Img16.Source = hordeSquare.Source;
+            Img17.Source = hordeSquare.Source;
+            Img18.Source = hordeSquare.Source;   
         }  
         
         private void WarchiefUnit_Click(object sender, RoutedEventArgs e)
@@ -92,6 +112,17 @@ namespace OOPGame_WoWChess
             mediaPlayer.Open(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/squireVoice.mp3"));
             mediaPlayer.Play();
         }
-        
+
+        private void WarchiefUnit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image warchief = new Image();
+            warchief.Source = new BitmapImage(new Uri(@"http://vidimitrov.com/images/Warcraft_Units/warchief.png", UriKind.Absolute));
+            this.BigCardImage.Width = 330;
+            this.BigCardImage.Source = warchief.Source;
+        }   
+        private void WarchiefUnit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.BigCardImage.Source = null;           
+        }          
     }
 }
